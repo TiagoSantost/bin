40f5c3f8fb59495a5480f271f9a01cdb1f7f4607
TiagoSantost <89109699+TiagoSantost@users.noreply.github.com>
2022-07-03 22:33:42 -0300

    Add files via upload

---
+++ Jsf/src/main/java/repositorio/IDaoPessoaImpl.java
@@ -0,0 +1,29 @@
+package repositorio;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import entidades.Pessoa;
+import jpautil.JPAUtil;
+
+public class IDaoPessoaImpl implements IDaoPessoa {
+
+	@Override
+	public Pessoa consultarUsuario(String login, String senha) {
+		
+		Pessoa pessoa = null;
+		
+		
+		EntityManager entityManager = JPAUtil.getEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		
+		pessoa = (Pessoa) entityManager.createQuery("select p from Pessoa p where p.login = '" + login + "' and p.senha = '" + senha + "'").getSingleResult();
+		
+		entityTransaction.commit();
+		entityManager.close();
+		return pessoa;
+	}
+	
+	
+}
